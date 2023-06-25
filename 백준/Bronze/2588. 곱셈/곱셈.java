45998b306ad251c5f52e8359016d68b5aee8d67d
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-06-25 15:41:19 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String b = sc.next();
+
+        System.out.println(a * Character.getNumericValue(b.charAt(2)));
+        System.out.println(a * Character.getNumericValue(b.charAt(1)));
+        System.out.println(a * Character.getNumericValue(b.charAt(0)));
+        System.out.println(a * Integer.parseInt(b));
+
+    }
+}
