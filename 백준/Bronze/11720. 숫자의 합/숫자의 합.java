f707feadb16cefa738d52bcc9f8164fc1fb3dee5
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-06-27 13:52:03 +0900

    [Bronze IV] Title: 숫자의 합, Time: 132 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String n = br.readLine();
+        bw.write(String.valueOf(Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).sum()));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
