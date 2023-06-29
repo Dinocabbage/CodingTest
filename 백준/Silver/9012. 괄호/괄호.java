2cadb122d7cf2cc90c37e4e72b1d1bbf59d73e9b
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-06-29 12:27:26 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -8,10 +8,12 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        
+
+        Stack<Character> s = new Stack<>();
+
         for(int i = 0; i < T; i++) {
-            Stack<Character> s = new Stack<>();
-            
+            s.clear();
+
             for(char c : br.readLine().toCharArray()) {
                 if(c == '(') {
                     s.push(c);
