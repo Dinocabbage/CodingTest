b4cd0f50ed3da4d692aec231bd8ba180b09db4db
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-07-06 01:32:58 +0900

    [Silver IV] Title: 듣보잡, Time: 344 ms, Memory: 27052 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -16,14 +16,14 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        StringBuilder sb = new StringBuilder();
+
 //      하나의 배열에 다 받아서 2회 이상 등장하는 원소만 뽑아내는 법
-        String[] input = new String[N + M];
+        /*String[] input = new String[N + M];
 
         for(int i = 0; i < N + M; i++) {
             input[i] = br.readLine();
         }
-
-        StringBuilder sb = new StringBuilder();
 
         List<String> answer = Arrays.stream(input)
                 .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
@@ -32,6 +32,27 @@ public class Main {
                 .filter(entry -> entry.getValue() > 1)
                 .map(Map.Entry::getKey)
                 .map(String::valueOf)
+                .sorted()
+                .collect(Collectors.toList());
+
+        sb.append(answer.size() + "\n");
+        sb.append(answer.stream().collect(Collectors.joining("\n")));*/
+
+//      두 배열에 나눠 받아서 중복되는 원소만 뽑아내는 법
+        String[] input1 = new String[N];
+
+        for(int i = 0; i < N; i++) {
+            input1[i] = br.readLine();
+        }
+
+        String[] input2 = new String[M];
+
+        for(int i = 0; i < M; i++) {
+            input2[i] = br.readLine();
+        }
+
+        List<String> answer = Arrays.stream(input1)
+                .filter(Arrays.stream(input2).collect(Collectors.toSet())::contains)
                 .sorted()
                 .collect(Collectors.toList());
 
