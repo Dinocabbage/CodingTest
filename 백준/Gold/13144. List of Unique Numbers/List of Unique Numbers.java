5b8a536737f9c48c7e9b49137e7466f7b4fb2353
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-07-08 14:53:05 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 320 ms, Memory: 27020 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,42 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
- 
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
- 
-    static int N;
-    static long count;
-    static int[] numbers;
-    static boolean[] isUsed;
- 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        numbers = new int[N];
-        isUsed = new boolean[100001];
- 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
- 
-        countNumberOfCases();
- 
-        System.out.println(count);
-    }
- 
-    public static void countNumberOfCases() {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        boolean[] isUsed = new boolean[100001];
+        long count = 0;
+
         for(int left = 0, right = -1; left < N; left++) {
             while (right + 1 < N && !isUsed[numbers[right + 1]]) {
                 right++;
                 isUsed[numbers[right]] = true;
             }
- 
+
             count += right - left + 1;
- 
+
             isUsed[numbers[left]] = false;
         }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
     }
+
 }
