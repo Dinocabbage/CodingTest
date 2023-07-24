aab41732dd2129b5b951892f0a8f0b5741b757c8
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-07-24 14:54:33 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 220 ms, Memory: 17108 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                arr[i][j] += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            bw.write(Arrays.stream(arr[i]).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
