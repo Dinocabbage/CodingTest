ab958a78fac21d35e182b1e2e2b3389f0db4d595
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-08-09 18:31:01 +0900

    [Gold V] Title: 수 고르기, Time: 1528 ms, Memory: 32108 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(A);
+
+        int lt = 0;
+        int answer = Integer.MAX_VALUE;
+
+        while(lt < N) {
+            for(int rt = lt + 1; rt < N; rt++) {
+                if(A[rt] - A[lt] >= M) {
+                    answer = Math.min(answer, A[rt] - A[lt]);
+                    break;
+                }
+            }
+
+            lt++;
+        }
+
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
