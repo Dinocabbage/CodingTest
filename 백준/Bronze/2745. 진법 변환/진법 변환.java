741bcaeb62867a1ec9843f175a02a1ee9e97a099
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-08-11 02:55:51 +0900

    [Bronze II] Title: 진법 변환, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(Integer.parseInt(N, B)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
