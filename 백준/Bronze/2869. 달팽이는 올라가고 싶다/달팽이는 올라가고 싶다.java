f17d94a56bc98993d527d490557acd9ab7324258
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-08-11 04:10:02 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf((V - B - 1) / (A - B) + 1));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
