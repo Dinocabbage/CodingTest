16cc576bcea1f6ed25949afee04ce18a69cbe281
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-08-17 12:01:55 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) {
+                break;
+            }
+
+            if(b % a == 0) {
+                bw.write("factor");
+            }
+            else if(a % b == 0) {
+                bw.write("multiple");
+            }
+            else {
+                bw.write("neither");
+            }
+
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
