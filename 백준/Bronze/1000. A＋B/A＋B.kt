f1350f0d436aaaa48d7d54904bb45dae36b9840a
Dinocabbage <122075305+Dinocabbage@users.noreply.github.com>
2023-08-26 22:10:26 +0900

    [Bronze V] Title: A+B, Time: 136 ms, Memory: 20728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write(br.readLine().split(" ").map { it.toInt() }.sum().toString())
+    bw.flush()
+    bw.close()
+    br.close()
+}
